d76a9d61964b9d697419342a2a3957d7b687a40d
Jiekai Sun <jiekai.sun@studio.unibo.it>
2024-10-06 17:11:37 +0200

    Partial implementation of timers

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <LiquidCrystal_I2C.h>
+#include <TimerOne.h>
 
 /*
   Give Me The Binary!
@@ -39,6 +40,12 @@
 #define MAX_TIME_WINDOW 10
 #define RED_LED_BRIGHTNESS_STEP 5
 
+//Difficulties
+#define VERY_EASY 0
+#define EASY 1
+#define NORMAL 2
+#define HARD 3
+
 //Game variables
 int state;
 int difficulty;
@@ -54,6 +61,8 @@ int redLedBrightnessStep;
 int leds[LED_COUNT - 1];
 bool ledStates[LED_COUNT - 1];
 int buttons[BUTTON_COUNT - 1];
+
+bool timeWindowElapsed;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // 0x27 is the address for most I2C LCD displays
 
@@ -91,6 +100,8 @@ void setup() {
 
   lcd.init();
   lcd.backlight();
+
+  timeWindowElapsed = false;
   state = SETUP1;
 }
 
@@ -115,37 +126,41 @@ void loop() {
 
 void setup1() {
   score = 0;
-  difficulty = 1;
+  difficulty = 0;
+  currentMaxTime = MAX_TIME_WINDOW;
+  lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("Welcome");
+  lcd.println("Welcome to GMB!");
   delay(1000);
   //start timer and transition to next state
-  state = SETUP2; 
+  state = SETUP2;
+  startTimer(1000000);
 }
 
 void setup2() {
-  int potentiometerValue = analogRead(POTENTIOMETER_PIN);
-  difficulty = mapToDifficulty(potentiometerValue);
-  currentMaxTime = MAX_TIME_WINDOW - TIME_DELTA * difficulty;
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Difficulty: X Turn Time: Y");
-  lcd.setCursor(0,1);
-  lcd.print("Press B1 to Start");
-  if (redLedBrightness < 0 || redLedBrightness > 255) {
-    redLedBrightnessStep = -redLedBrightnessStep;
+  //if the timer this time around has fully elapsed then go into deep sleep
+  if (timeWindowElapsed) {
+    //sleep
+  } else {
+    int potentiometerValue = analogRead(POTENTIOMETER_PIN);
+    difficulty = mapToDifficulty(potentiometerValue);
+    currentMaxTime = MAX_TIME_WINDOW - TIME_DELTA * difficulty;
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Difficulty: X Turn Time: Y");
+    lcd.setCursor(0,1);
+    lcd.print("Press B1 to Start");
+    if (redLedBrightness < 0 || redLedBrightness > 255) {
+      redLedBrightnessStep = -redLedBrightnessStep;
+    }
+    redLedBrightness = redLedBrightness + redLedBrightnessStep;
+    analogWrite(RED_LED_PIN, redLedBrightness);
+    
+    //if b1 is pressed in time go to next state
+    if (digitalRead(buttons[0] == HIGH)) {
+      state = GAMING1;
+    }
   }
-  redLedBrightness = redLedBrightness + redLedBrightnessStep;
-  analogWrite(RED_LED_PIN, redLedBrightness);
-  
-  //if b1 is pressed in time go to next state else force sleep.
-  if (digitalRead(buttons[0] == HIGH)) {
-    state = GAMING1;
-  }
-}
-
-void wake() {
-  state = SETUP1;
 }
 
 void gaming1() {
@@ -155,83 +170,102 @@ void gaming1() {
   currentRandomValue = rand() % MAX_RANDOM + 1;
   //start timer and transition to the next state.
   state = GAMING2;
+  startTimer(currentMaxTime * 1000000);
 }
 
 void gaming2() {
-  for (size_t i = 0; i < BUTTON_COUNT - 1; i++)
-  {
-    if(digitalRead(buttons[i]) == HIGH) {
-      digitalWrite(leds[i], HIGH);
-      ledStates[i] = true;
-    } else {
-      digitalWrite(leds[i], LOW);
-      ledStates[i] = false;
-    }
-  }
-  int sum = 0;
-  for (size_t i = 0; i < LED_COUNT - 1; i++)
-  {
-    if (ledStates[i] == true) {
-      switch (i) {
-        case 0:
-          sum = sum + 1;
-          break;
-        case 1:
-          sum = sum + 2;
-          break;
-        case 2:
-          sum = sum + 4;
-          break;
-        case 3:
-          sum = sum + 8;
-          break;
-        default:
-          sum = sum + 0;
-          break;
+  if (timeWindowElapsed) {
+    state = GAMING4;
+  } else {
+    for (size_t i = 0; i < BUTTON_COUNT - 1; i++)
+      {
+        if(digitalRead(buttons[i]) == HIGH) {
+          digitalWrite(leds[i], HIGH);
+          ledStates[i] = true;
+        } else {
+          digitalWrite(leds[i], LOW);
+          ledStates[i] = false;
+        }
+      }
+      int sum = 0;
+      for (size_t i = 0; i < LED_COUNT - 1; i++)
+      {
+        if (ledStates[i] == true) {
+          switch (i) {
+            case 0:
+              sum = sum + 1;
+              break;
+            case 1:
+              sum = sum + 2;
+              break;
+            case 2:
+              sum = sum + 4;
+              break;
+            case 3:
+              sum = sum + 8;
+              break;
+            default:
+              sum = sum + 0;
+              break;
+          }
+        }
+      if (sum == currentRandomValue) {
+          state = GAMING3;
       }
     }
-  }
-  if (sum == currentRandomValue) {
-    //Stop timer and transition to next stage
-    state = GAMING3;
-  }
-  //Timer runs out, transition to game over
-  state = GAMING4;
+  } 
 }
 
 void gaming3() {
-  lcd.clear();
   score++;
-  lcd.setCursor(0,0);
-  lcd.print("Score " + score);
   currentMaxTime = currentMaxTime - TIME_DELTA;
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.println("You won this round");
+  lcd.setCursor(0,1);
+  lcd.println("Score " + String(score));
   delay(2000);
-  state = GAMING2;
+  state = GAMING1;
 }
 
 void gaming4() {
   lcd.clear();
-  //turn red light on and off in the span of 1 sec
   digitalWrite(RED_LED_PIN, HIGH);
   delay(1000);
   digitalWrite(RED_LED_PIN, LOW);
-  //print results
   lcd.setCursor(0,0);
-  lcd.print("Game Over");
+  lcd.println("Game Over!");
   lcd.setCursor(0,1);
-  lcd.print("Score " + score);
+  lcd.println("Score " + String(score));
   delay(10000);
   state = SETUP1;
 }
 
 int mapToDifficulty(int value) {
     if (value <= 255) {
-        return 0;  // Difficulty Level 0
+        return VERY_EASY;
     } else if (value <= 511) {
-        return 1;  // Difficulty Level 1
+        return EASY;
     } else if (value <= 767) {
-        return 2;  // Difficulty Level 2
+        return NORMAL;
     } else {
-        return 3;  // Difficulty Level 3
+        return HARD;
     }
+}
+
+void startTimer(unsigned long seconds) {
+  Timer1.stop();
+  timeWindowElapsed = false;
+  Timer1.initialize(seconds);
+  Timer1.attachInterrupt(timerISR);
+  Timer1.start();
+}
+
+void timerISR() {
+  timeWindowElapsed = true;
+  Timer1.stop();
+}
+
+void wake() {
+  state = SETUP1;
 }
